09b1b89957f0660d8292835e4ba869f1889bf5ee
RAJ-0727 <146083756+RAJ-0727@users.noreply.github.com>
2023-10-04 11:41:51 +0000

    Wed, Oct 4, 2023, 11:41 AM +00:00

--- compare.c
+++ compare.c
@@ -3,19 +3,14 @@
 
 int main(void)
 {
-  int x = get_int("what's x? ");
-  int y = get_int("what's y? ");
+    char c = get_char("do you agree? ");
 
-  if (x<y)
-  {
-    printf("x is less than y\n");
-  }
-  else if (x>y)
-  {
-    printf("y is less than x\n");
-  }
-  else
-  {
-    printf("x is equal to y\n");
-  }
+    if (c == 'y')
+    {
+        printf("Agreed.\n");
+    }
+    else if (c == 'N')
+    {
+        printf("Not Agreed.\n");
+    }
 }
